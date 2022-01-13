14fbb3158475511232a06ddbf0a1d96947232330
user_nitro_5 <timmy.lepel@gmail.com>
2022-01-13 23:29:09 +0300

    add projects

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import s from './Footer.module.scss'
+import {Title} from '../common/components/title/Title'
 import telegramIcon from '../assets/images/telegram.svg'
 import likedInIcon from './../assets/images/linkedin.svg'
 import vkIcon from './../assets/images/vk.svg'
 import gitHubIcon from '../assets/images/gitHubIcon.png'
-import {Title} from '../common/components/title/Title'
+import instImg from '../assets/images/instagram.svg'
+
 
 
 
@@ -18,25 +20,31 @@ export const Footer = () => {
                     <div className={s.socialIcon}>
                         <a target={'blank'}
                            href="https://t.me/smazhevskiy_a">
-                            <img src={telegramIcon} alt=""/>
+                            <img src={telegramIcon} alt="telegram"/>
                         </a>
                     </div>
                     <div className={s.socialIcon}>
                         <a target={'blank'}
                            href="https://vk.com/smazhevskiy_a">
-                            <img src={vkIcon} alt=""/>
+                            <img src={vkIcon} alt="vk"/>
                         </a>
                     </div>
                     <div className={s.socialIcon}>
                         <a target={'blank'}
                            href="https://www.linkedin.com/in/artem-zmazhevskiy-330a40215/">
-                            <img src={likedInIcon} alt=""/>
+                            <img src={likedInIcon} alt="linkedIn"/>
                         </a>
                     </div>
                     <div className={s.socialIcon}>
                         <a target={'blank'}
                            href="https://github.com/Smazhevskiy">
-                            <img src={gitHubIcon} alt=""/>
+                            <img src={gitHubIcon} alt="gitHub"/>
+                        </a>
+                    </div>
+                    <div className={s.socialIcon}>
+                        <a target={'blank'}
+                           href="http://instgram.ru/artemsmazheuski">
+                            <img src={instImg} alt="inst"/>
                         </a>
                     </div>
                 </div>
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -5,6 +5,9 @@ import socialImage from '../assets/images/socialImg.png'
 import cardsImg from '../assets/images/cardsImg.png'
 import {Title} from '../common/components/title/Title'
 import {Project} from './project/Project'
+import booksImg from '../assets/images/searchBooksImg.png'
+import landingImg from '../assets/images/landingImg.png'
+import gradientsImg from '../assets/images/gradients.png'
 
 
 
@@ -20,6 +23,19 @@ export const Projects = () => {
         backgroundImage: `url(${cardsImg})`,
     }
 
+    const books = {
+        backgroundImage: `url(${booksImg})`,
+    }
+
+    const landing = {
+        backgroundImage: `url(${landingImg})`,
+    }
+
+    const gradients = {
+        backgroundImage: `url(${gradientsImg})`,
+    }
+
+
     return (
         <div id="projects" className={s.projectsBlock}>
             <div className={s.container}>
@@ -30,17 +46,37 @@ export const Projects = () => {
                              hrefDemo={'https://smazhevskiy.github.io/socialNetwork'}
                              hrefCode={'https://github.com/Smazhevskiy/socialNetwork'}
                              description={'A social networking service. An online platform for people to build ' +
-                             'social networks or social relationships with other people.'}/>
+                             'social networks or social relationships with other people.'}
+                    />
                     <Project style={todolist}
                              title={'Todolist'}
                              hrefDemo={'https://smazhevskiy.github.io/toDoList'}
                              hrefCode={'https://github.com/Smazhevskiy/toDoList'}
-                             description={'App for Task Management, Project Management, Productivity. '}/>
+                             description={'App for Task Management, Project Management, Productivity. '}
+                    />
                     <Project style={cards}
                              title={'Cards'}
                              hrefDemo={'https://smazhevskiy.github.io/learningCards_App'}
                              hrefCode={'https://github.com/Smazhevskiy/learningCards_App'}
-                             description={'Here you can create your own card packs with questions and answers. Then you can check your skills trying to give give answers to cards and grading your success or failure. Future cards will be offered based on personal grade.'}/>
+                             description={'Here you can create your own card packs with questions and answers.'}
+                    />
+                    <Project style={books}
+                             title={'Search books'}
+                             hrefDemo={'https://smazhevskiy.github.io/googleBooks'}
+                             hrefCode={'https://github.com/Smazhevskiy/googleBooks'}
+                             description={'App for searching books bu goole API.'}
+                    />
+                    <Project style={gradients}
+                             title={'Gradients'}
+                             hrefDemo={'https://smazhevskiy.github.io/VertageLabTest'}
+                             hrefCode={'https://github.com/Smazhevskiy/VertageLabTest'}
+                             description={'Mixing two colors in #hex format with the ability to edit, delete colors and validate for a color code'}
+                    />
+                    <Project style={landing}
+                             title={'Simple landing'}
+                             hrefDemo={'https://smazhevskiy.github.io/layout-Freelance'}
+                             hrefCode={'https://github.com/Smazhevskiy/layout-Freelance'}
+                    />
                 </div>
             </div>
         </div>
